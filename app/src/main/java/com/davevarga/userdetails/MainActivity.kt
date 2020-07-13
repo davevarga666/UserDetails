30a37cb8eadb2146d11749e73dbd1df742dea559
davevarga666 <davevarga999@gmail.com>
2020-07-13 11:49:12 +0200

    UI updated.

--- app/src/main/java/com/davevarga/userdetails/MainActivity.kt
+++ app/src/main/java/com/davevarga/userdetails/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.davevarga.userdetails
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
