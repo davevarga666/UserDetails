9274e112a714c43d7dfe8c24343b0dc36395a108
davevarga666 <davevarga999@gmail.com>
2020-07-13 22:53:20 +0200

    UI updated.

--- app/src/main/java/com/davevarga/userdetails/DetailsActivity.kt
+++ app/src/main/java/com/davevarga/userdetails/DetailsActivity.kt
@@ -2,6 +2,7 @@ package com.davevarga.userdetails
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_detail.*
 
@@ -11,13 +12,13 @@ class DetailsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_detail)
 
         val user = intent.getParcelableExtra<User>("userDetails")
-        textView_name.text = user?.name
-        textView_phone.text = user?.phone
-        textView_address.text = user?.address
-        textView_city.text = user?.city
-        textView_city.text = user?.zip
-        textView_email.text = user?.email
-        textView_birthday.text = user?.birthday
+        textView_name.setText(user?.name)
+        textView_phone.setText(user?.phone)
+        textView_address.setText(user?.address)
+        textView_city.setText(user?.city)
+        textView_zip.setText(user?.zip)
+        textView_email.setText(user?.email)
+        textView_birthday.setText(user?.birthday)
 
     }
 }
--- app/src/main/java/com/davevarga/userdetails/MainActivity.kt
+++ app/src/main/java/com/davevarga/userdetails/MainActivity.kt
@@ -7,6 +7,9 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
@@ -17,14 +20,15 @@ class MainActivity : AppCompatActivity() {
 
         val btn_save = findViewById(R.id.saveButton) as Button
 
+
         btn_save.setOnClickListener {
-            val myName = findViewById<EditText>(R.id.name).text.toString()
-            val myPhone = findViewById<EditText>(R.id.phone).text.toString()
-            val myAddress = findViewById<EditText>(R.id.address).text.toString()
-            val myCity = findViewById<EditText>(R.id.city).text.toString()
-            val myZip = findViewById<EditText>(R.id.zip).text.toString()
-            val myEmail = findViewById<EditText>(R.id.email).text.toString()
-            val myBirthday = findViewById<EditText>(R.id.birthday).text.toString()
+            val myName = findViewById<TextInputEditText>(R.id.editName).text.toString()
+            val myPhone = findViewById<TextInputEditText>(R.id.editPhone).text.toString()
+            val myAddress = findViewById<TextInputEditText>(R.id.editAddress).text.toString()
+            val myCity = findViewById<TextInputEditText>(R.id.editCity).text.toString()
+            val myZip = findViewById<TextInputEditText>(R.id.editZip).text.toString()
+            val myEmail = findViewById<TextInputEditText>(R.id.editEmail).text.toString()
+            val myBirthday = findViewById<TextInputEditText>(R.id.editBirthday).text.toString()
 
             val user = User(myName, myPhone, myAddress, myCity, myZip, myEmail, myBirthday)
             val intent =
